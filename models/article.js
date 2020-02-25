d47a528040030ff5d4c4b245f4ee2742da67a537
Brandon Wie <it.brandonwie@gmail.com>
2020-02-25 18:04:45 +0100

    first commit

---
+++ models/article.js
@@ -0,0 +1,38 @@
+"use strict";
+const Sequelize = require("sequelize");
+const moment = require("moment");
+
+module.exports = (sequelize) => {
+  class Article extends Sequelize.Model {
+    publishedAt() {
+      const date = moment(this.createdAt).format(
+        "MMMM D, YYYY, h:mma"
+      );
+      return date;
+    }
+    shortDescription() {
+      const shortDesc =
+        this.body.length > 200
+          ? this.body.substring(0, 200) + "..."
+          : this.body;
+      return shortDesc;
+    }
+  }
+  Article.init(
+    {
+      title: {
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: {
+            msg: '"Title" is required',
+          },
+        },
+      },
+      author: Sequelize.STRING,
+      body: Sequelize.TEXT,
+    },
+    { sequelize }
+  );
+
+  return Article;
+};
---
+++ routes/articles.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const router = express.Router();
+const Article = require("../models").Article;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
+
+/* GET articles listing. */
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.render("articles/index", {
+      articles: {},
+      title: "Sequelize-It!",
+    });
+  })
+);
+
+/* Create a new article form. */
+router.get("/new", (req, res) => {
+  res.render("articles/new", {
+    article: {},
+    title: "New Article",
+  });
+});
+
+/* POST create article. */
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    article = await Article.create(req.body);
+    res.redirect("/articles/" + article.id);
+  })
+);
+
+/* Edit article form. */
+router.get(
+  "/:id/edit",
+  asyncHandler(async (req, res) => {
+    const article = await Article.findByPk(
+      req.params.id
+    );
+    if (article) {
+      res.render("articles/edit", {
+        article,
+        title: "Edit Article",
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* GET individual article. */
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const article = await Article.findByPk(
+      req.params.id
+    );
+    if (article) {
+      res.render("articles/show", {
+        article,
+        title: article.title,
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* Update an article. */
+router.post(
+  "/:id/edit",
+  asyncHandler(async (req, res) => {
+    let article;
+    try {
+      article = await Article.findByPk(
+        req.params.id
+      );
+      if (article) {
+        await article.update(req.body);
+        res.redirect("/articles/" + article.id);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError"
+      ) {
+        article = await Article.build(req.body);
+        article.id = req.params.id;
+        res.render("articles/edit", {
+          article,
+          errors: error.errors,
+          title: "Edit Article",
+        });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+/* Delete article form. */
+router.get(
+  "/:id/delete",
+  asyncHandler(async (req, res) => {
+    const article = await Article.findByPk(
+      req.params.id
+    );
+    if (article) {
+      res.render("articles/delete", {
+        article,
+        title: "Delete Article",
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* Delete individual article. */
+router.post(
+  "/:id/delete",
+  asyncHandler(async (req, res) => {
+    const article = await Article.findByPk(
+      req.params.id
+    );
+    if (article) {
+      await article.destroy();
+      res.redirect("/articles");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+module.exports = router;
